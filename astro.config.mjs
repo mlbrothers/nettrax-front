4fd03f20dc17671a00abfbdd9cdac580096bc718
Viraj Bhagiya <virajbhagiya@gmail.com>
2026-09-25 15:20:59 +0530

    Add: Base Version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      allowedHosts: [
+        "nonimpressionable-unnotionally-ollie.ngrok-free.dev"
+      ],
+    },
+    plugins: [tailwindcss()],
+  },
+});
